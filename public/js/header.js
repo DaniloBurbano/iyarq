9e416a0900d2c6013b9b899d41ec5893a7966e33
Danilo Burbano <danilo.b.argoty@gmail.com>
2024-07-29 00:05:06 -0500

    Issues in desktop view were fixed

--- public/js/header.js
+++ public/js/header.js
@@ -3,7 +3,7 @@ let prevScrollPos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
   console.log("ypos: " + currentScrollPos);
-  if (prevScrollPos > currentScrollPos && currentScrollPos < 1000) {
+  if (prevScrollPos > currentScrollPos && currentScrollPos < 960) {
     document.querySelector('.main-navbar').classList.remove('main-navbar-color');
     
   } else {
