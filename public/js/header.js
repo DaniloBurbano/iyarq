00441f0fb4284a537b4daa7c2b9d300c18164310
Danilo Burbano <danilo.b.argoty@gmail.com>
2024-08-01 21:52:31 -0500

    Mobile header was created

--- public/js/header.js
+++ public/js/header.js
@@ -2,13 +2,18 @@ let prevScrollPos = window.pageYOffset;
 
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
-  console.log("ypos: " + currentScrollPos);
+
+  if (prevScrollPos > currentScrollPos && currentScrollPos < 200) {
+    document.querySelector('.main-navbar-mobile').classList.remove('main-navbar-color');
+  }
+  else {
+    document.querySelector('.main-navbar-mobile').classList.add('main-navbar-color');
+  }
+
   if (prevScrollPos > currentScrollPos && currentScrollPos < 960) {
-    document.querySelector('.main-navbar').classList.remove('main-navbar-color');
-    
+    document.querySelector('.main-navbar').classList.remove('main-navbar-color');    
   } else {
     document.querySelector('.main-navbar').classList.add('main-navbar-color');
-    
   }
   prevScrollPos = currentScrollPos;
 }
