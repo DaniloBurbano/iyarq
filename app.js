0ba908b7e6f953c240d8e685549970f274753622
Danilo Burbano <danilo.b.argoty@gmail.com>
2024-08-07 21:04:55 -0500

    Last details were added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', path.resolve(__dirname, "./views/"));
 
 app.get("/", (req, res) => {
   res.render('index', {
-    title: "INGENIERIA Y ARQUITECTURA",
+    title: "INGENIERÍA & ARQUITECTURA",
     stylesheetFile: "index.css"
   })
 });
